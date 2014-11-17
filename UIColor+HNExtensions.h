9a40c18b6a032a4f24ea60740dacc017aa26286f
Henri Normak <henri.normak@gmail.com>
2014-11-17 19:48:31 +0000

    Add comments to method declarations
    Some methods are obvious, while others now have a better explanation of their inner workings.

--- UIColor+HNExtensions.h
+++ UIColor+HNExtensions.h
@@ -32,69 +32,166 @@
 #pragma mark -
 #pragma mark Components
 
+/**
+ *  @return Alpha component of the color
+ */
 - (CGFloat)getAlpha;
 
+/**
+ *  @return Red component of the color, 0 for non RGB color
+ */
 - (CGFloat)getRed;
+
+/**
+ *  @return Green component of the color, 0 for non RGB color
+ */
 - (CGFloat)getGreen;
+
+/**
+ *  @return Blue component of the color, 0 for non RGB color
+ */
 - (CGFloat)getBlue;
 
+/**
+ *  @return Hue component of the color, 0 for non HSB color
+ */
 - (CGFloat)getHue;
+
+/**
+ *  @return Saturation component of the color, 0 for non HSB color
+ */
 - (CGFloat)getSaturation;
+
+/**
+ *  @return Brightness component of the color, 0 for non HSB color
+ */
 - (CGFloat)getBrightness;
 
+/**
+ *  @return Calculated luminance for the colour, works with both RGB and grayscale
+ */
 - (CGFloat)getLuminance;
 
-// YES if this colour was likely created by +colorWithPatternImage:
+/**
+ *  @return YES if this colour was likely created by +colorWithPatternImage:
+ */
 - (BOOL)isPatternBased;
 
-// Comparison to another UIColor, converts both to suitable model/space beforehand
+/**
+ *  Comparison to another UIColor, converts both to suitable model/space beforehand
+ *
+ *  @param color to compare against the receiver
+ *
+ *  @return YES if the colors are equal (their components are equal)
+ */
 - (BOOL)isEqualToColor:(UIColor *)color;
 
-// Following methods take value from 0-1 and replace the receiver's corresponding value
+/**
+ *  @param hue New hue value for the color
+ *
+ *  @return Color with the hue component replaced
+ */
 - (UIColor *)colorWithHue:(CGFloat)hue;
+
+/**
+ *  @param saturation New saturation value for the color
+ *
+ *  @return Color with the saturation component replaced
+ */
 - (UIColor *)colorWithSaturation:(CGFloat)saturation;
+
+/**
+ *  @param brightness New brightness value for the color
+ *
+ *  @return Color with the brightness component replaced
+ */
 - (UIColor *)colorWithBrightness:(CGFloat)brightness;
 
 #pragma mark -
 #pragma mark Dimming
 
-// iOS 7 like dimming of a colour
+/**
+ *  @return iOS 7 style dimmed version of the color, i.e what happens to tint when an alert is shown
+ */
 - (UIColor *)dimmedColor;
 
 #pragma mark -
 #pragma mark Color palette
 
-// Complementary color
+/**
+ *  @return Complementary color to the receiver
+ */
 - (UIColor *)complementaryColor;
 
-// Following methods return an array of 3 UIColor objects (first of which is the receiver)
+/**
+ *  @return Array of 3 UIColor objects that are analogous to the receiver
+ */
 - (NSArray *)analogousColors;
+
+/**
+ *  @return Array of 3 UIColor objects that make the split complementary palette for the receiver
+ */
 - (NSArray *)splitComplementaryColors;
+
+/**
+ *  @return Array of 3 UIColor objects that represent the triadic palette for the receiver
+ */
 - (NSArray *)triadicColors;
 
-// Following methods return an array of 4 UIColor objects (first of which is the receiver)
+/**
+ *  @return Array of 4 UIColors that form the tetradic palette with the receiver
+ */
 - (NSArray *)tetradicColors;
+
+/**
+ *  @return Array of 4 UIColors that form square palette with the receiver on the color wheel
+ */
 - (NSArray *)squareColors;
 
 #pragma mark -
 #pragma mark Accessibility
 
-// Returns either black or white, whichever has better contrast with the color
+/**
+ *  @return Either black or white, whichever has better contrast with the color
+ */
 - (UIColor *)contrastingTextColor;
 
-// Measured according to the WGAC standard, returns a ratio >= 1.0
+/**
+ *  @param color Color to compare against
+ *
+ *  @return Ratio according to the WGAC standard, >= 1.0
+ */
 - (CGFloat)contrastRatioWithColor:(UIColor *)color;
 
-// WGAC AAA standard is assumed, the ratio has to be better than 4.5
+/**
+ *  @param color Color to compare against
+ *
+ *  @return WGAC AAA standard is assumed, YES if ratio is better than 4.5
+ */
 - (BOOL)isAccessibleWithBackgroundColor:(UIColor *)color;
 
 #pragma mark -
 #pragma mark Gradients
 
-// Position is a unit value between 0.0 and 1.0
+/**
+ *  Calculate a color in a gradient at a position between two colors
+ *
+ *  @param position  Unit value between 0.0 and 1.0 (enforced)
+ *  @param fromColor Start color for the gradient
+ *  @param toColor   End color for the gradient
+ *
+ *  @return Color at the given position between the two colors
+ */
 + (UIColor *)colorAtPosition:(CGFloat)position fromColor:(UIColor *)fromColor toColor:(UIColor *)toColor;
 
-// Wrapper around the previous call, useful for multicolor gradients
+/**
+ *  Derives the colour in a linear gradient consisting of n colours
+ *
+ *  @param position Unit value between 0.0 and 1.0 (enforced)
+ *  @param colors   Array of colours that form the linear uniform gradient
+ *
+ *  @return Color at the given position in the gradient
+ */
 + (UIColor *)colorAtPosition:(CGFloat)position withinColors:(NSArray *)colors;
 
 #pragma mark -
@@ -115,8 +212,25 @@ typedef enum {
     kColorBlendingModeExclusion,
 } UIColorBlendingMode;
 
-// Blending two colors based on a rule
+/**
+ *  Blend the receiver with another color to using a specified blending mode
+ *
+ *  @param source Colour to add (the order does matter, receiver is the destination)
+ *  @param mode   Blending mode
+ *
+ *  @return Result from blending receiver with source
+ */
 - (UIColor *)colorByBlendingWithColor:(UIColor *)source mode:(UIColorBlendingMode)mode;
+
+/**
+ *  Blend the receiver with another color to using a specified blending mode at a given alpha
+ *
+ *  @param source Colour to add (the order does matter, receiver is the destination)
+ *  @param mode   Blending mode
+ *  @param alpha  Alpha to apply to the source colour (in addition to its natural alpha component)
+ *
+ *  @return Result from blending receiver with source
+ */
 - (UIColor *)colorByBlendingWithColor:(UIColor *)source mode:(UIColorBlendingMode)mode alpha:(CGFloat)alpha;
 
 #pragma mark -
@@ -126,13 +240,38 @@ typedef enum {
 // If less than six characters long, will be used as a pattern - "FFA" will result in "FFAFFA" and "FFFA" results in "FFFAFF"
 // Does not take alpha into account (i.e alpha is always set to 100%)
 // Additionally works with the constants used by UIColor, such as "yellow" or "orange" or "clear"
+/**
+ *  Convert hex string into a UIColor object
+ *  If less than six characters long, will be used as a pattern - "FFA" will result in "FFAFFA" and "FFFA" results in "FFFAFF"
+ *  Does not take alpha into account (i.e alpha is always set to 100%)
+ *  Additionally works with the constants used by UIColor, such as "yellow" or "orange" or "clear"
+ *
+ *  @param colorCode Color code as a string, refer to brief for potential values
+ *
+ *  @return UIColor corresponding to the code
+ */
 + (UIColor *)colorForHexString:(NSString *)colorCode;
+
+/**
+ *  Hex string from the color
+ *
+ *  @return hex color code for the receiver
+ */
 - (NSString *)hexString;
 
-// Random color generator
+/**
+ *  @return Random UIColor, generated using arc4random() used in RGB
+ */
 + (UIColor *)randomColor;
 
-// Color from string, uses SHA1 of the string to get a list of colours, then picks the most saturated one
+/**
+ *  Color from string, uses SHA1 of the string to get a list of colours, then picks the most saturated one
+ *
+ *  @param string String to seed the colour generator with
+ *  @param color  Colour to match the hue against, pass nil if no preference
+ *
+ *  @return Color derived from the string
+ */
 + (UIColor *)colorForString:(NSString *)string withIdeal:(UIColor *)color;
 
 @end
